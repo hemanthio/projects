11cb6acb95c2fe44d738412dcb6e1661ef519a45
hemanthio <121523327+hemanthio@users.noreply.github.com>
2024-03-08 20:54:15 +0530

    work without animation

--- Refokus/src/App.jsx
+++ Refokus/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Work from './components/Work'
 
 function App() {
   return (
     <div className='bg-[#161618] font-["satoshi variable"] m-0 p-0'>
    <Navbar/>
+   <Work/>
    </div>
   )
 }
